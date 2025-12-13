fdf8fe1100def18e7a0f02c70a901a87ef8f0f8e
N-Haritha16 <harithanallamilli9702@gmail.com>
2025-12-14 00:30:25 +0530

    Fix config.py

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = Path("/data")
 DATA_DIR.mkdir(exist_ok=True)
 
-SEED_PATH = DATA_DIR / "seed.enc"
+SEED_PATH = DATA_DIR / "seed.txt"
 
 STUDENT_PUBLIC_KEY = BASE_DIR / "student_public.pem"
 INSTRUCTOR_PUBLIC_KEY = BASE_DIR / "instructor_public.pem"
